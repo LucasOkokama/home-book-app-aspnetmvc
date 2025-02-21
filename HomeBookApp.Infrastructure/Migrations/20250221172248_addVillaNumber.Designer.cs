ea5951ba7e6e600f58553bb4dac8cd9edc97b634
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-02-21 15:58:05 -0300

    feat: create new table and migration

--- HomeBookApp.Infrastructure/Migrations/20250221172248_addVillaNumber.Designer.cs
+++ HomeBookApp.Infrastructure/Migrations/20250221172248_addVillaNumber.Designer.cs
@@ -4,6 +4,7 @@ using HomeBookApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeBookApp.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250221172248_addVillaNumber")]
+    partial class addVillaNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
