d6f02e2436c43cb4c76a7014cbe83d5d654b06a8
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-02-27 16:10:27 -0300

    feat: create amenity table, repository and add it to unitofwork

---
+++ HomeBookApp.Application/Common/Interfaces/IAmenityRepository.cs
@@ -0,0 +1,14 @@
+﻿using HomeBookApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBookApp.Application.Common.Interfaces
+{
+    public interface IAmenityRepository : IRepository<Amenity>
+    {
+        void Update(Amenity entity);
+    }
+}
---
+++ HomeBookApp.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,27 @@
+﻿using HomeBookApp.Application.Common.Interfaces;
+using HomeBookApp.Domain.Entities;
+using HomeBookApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeBookApp.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        
+        public void Update(Amenity entity)
+        {
+            _db.VillaNumbers.Update(entity);
+        }
+    }
+}
