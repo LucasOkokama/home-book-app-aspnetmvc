6cd9efa8e89bc38c45c422a806e04af17ee7ff1f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-01 22:52:46 -0300

    feat: add migration updatevillainfo

---
+++ HomeBookApp.Infrastructure/Migrations/20250302015035_UpdateVillaInfo.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeBookApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVillaInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "\\images\\VillaImage\\9a94b90b-f4de-48af-a2be-3062f46ed318.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\images\\VillaImage\\ea29a3ae-32cb-4587-adb3-b8d832f6e002.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "\\images\\VillaImage\\7cbe3bba-c159-47dd-9ee3-ab11fdb1e03f.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x400");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x401");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://placehold.co/600x402");
+        }
+    }
+}
